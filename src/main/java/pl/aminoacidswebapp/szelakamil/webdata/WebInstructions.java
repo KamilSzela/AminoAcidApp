4dc8f795aa6675bf76b295867f714dfd8a2fb966
Kamil Szela <kamil.szela.programista@gmail.com>
2022-10-04 10:04:16 +0200

    image problem - added mapping for static resources

---
+++ src/main/java/pl/aminoacidswebapp/szelakamil/webdata/WebInstructions.java
@@ -0,0 +1,18 @@
+package pl.aminoacidswebapp.szelakamil.webdata;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebInstructions implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/img/**"
+        ).addResourceLocations("classpath:/static/img/");
+    }
+}
